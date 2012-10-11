f878348f2db38a95edca5660dd77f94da9542cb2
Michael Drogalis <mjd3089@rit.edu>
2012-10-11 17:14:20 -0400

    Description and URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-cssgenbuild "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen plugin to generate stylesheets from cssgen"
+  :url "https://github.com/MichaelDrogalis/lein-cssgenbuild"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cssgen-rule-seq "0.2.7-SNAPSHOT"]
